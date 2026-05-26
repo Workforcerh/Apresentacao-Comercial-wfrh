f0961dd251be01efecc05718a48403cee67d51c4
WorkForce RH <workforcerh@email.com>
2026-05-26 19:30:25 -0300

    Ajusta config para deploy via Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Base URL para GitHub Pages: /nome-do-repositorio/
-  base: '/Apresentacao-Comercial-wfrh/',
+  base: '/',
 })
